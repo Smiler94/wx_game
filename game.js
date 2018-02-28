577ce6c0dcc376b8de466b486c2d3a3cefed3d4e
linzhen <273487984@qq.com>
2018-02-28 23:05:23 +0800

    整合

--- game.js
+++ game.js
@@ -10,44 +10,50 @@ import Bullet from './js/player/bullet.js'
 let ctx = canvas.getContext('2d')
 let bg = new BackGround(ctx)
 let player = new Player(ctx)
+let screenWidth = window.innerWidth
+let screenHeight = window.innerHeight
 let enemys = []
 let aniId = 0
 let frame = 0
 let score = 0
 
-let loop = function() {
-    // bg.update()
-    bg.render(ctx)
-    player.drawToCanvas(ctx)
-    if (frame % 20 == 0) {
-        player.shoot()
-    }
-    if (frame % 100 == 0) {
-        let enemy = new Enemy()
-        enemy.init(5)
-        enemys.push(enemy)
-    }
-    player.bullets.forEach((bullet) => {
-        bullet.update()
-        bullet.drawToCanvas(ctx)
-        enemys.forEach((enemy) => {
-            if (enemy.isCollideWith(bullet)) {
-                enemy.playAnimation()
-                score++
-                ctx.fillText(
-                    score, 10, 30
-                )
-            }
-        })
-    })
-    enemys.forEach((enemy) => {
-        if (enemy.isPlaying) {
-            enemy.aniRender(ctx)
-        }
-        enemy.update()
-        enemy.drawToCanvas(ctx)
-    })
+let atlas = new Image()
+atlas.src = 'images/Common.png'
 
+let loop = function() {
+//     // bg.update()
+//     bg.render(ctx)
+//     player.drawToCanvas(ctx)
+//     if (frame % 20 == 0) {
+//         player.shoot()
+//     }
+//     if (frame % 100 == 0) {
+//         let enemy = new Enemy()
+//         enemy.init(5)
+//         enemys.push(enemy)
+//     }
+//     player.bullets.forEach((bullet) => {
+//         bullet.update()
+//         bullet.drawToCanvas(ctx)
+//         enemys.forEach((enemy) => {
+//             if (enemy.isCollideWith(bullet)) {
+//                 enemy.playAnimation()
+//                 score++
+//             }
+//         })
+//     })
+//     enemys.forEach((enemy) => {
+//         if (enemy.isPlaying) {
+//             enemy.aniRender(ctx)
+//         }
+//         enemy.update()
+//         enemy.drawToCanvas(ctx)
+//     })
+// 	ctx.fillStyle = "#ffffff"
+// 	ctx.font = "20px Arial"
+// 	ctx.fillText(
+// 		score, 10, 30
+// 	)
     aniId = window.requestAnimationFrame(
         loop,
         canvas
--- js/main.js
+++ js/main.js
@@ -1,1 +1,72 @@
+import './js/libs/weapp-adapter.js'
+
+import Sprite from './js/base/sprite.js'
 import BackGround from './js/runtime/background.js'
+import Player from './js/player/index.js'
+import Enemy from './js/npc/enemy.js'
+import Bullet from './js/player/bullet.js'
+
+let ctx = canvas.getContext('2d')
+let screenWidth = window.innerWidth
+let screenHeight = window.innerHeight
+let frame = 0
+
+export default class Main {
+	constructor() {
+		this.aniId = 0
+		this.restart()
+	}
+
+	restart() {
+		this.bg = new BackGround(ctx)
+		this.player = new Player(ctx)
+		this.bindLoop = this.loop.bind(this)
+		this.enemys = []
+
+		this.aniId = window.requestAnimationFrame(
+			this.bindLoop,
+			canvas
+		)
+		
+	}
+
+	update() {
+		
+	}
+
+	bgUpdate() {
+		this.bg.update()
+	}
+
+	enemyUpdate() {
+		if (frame % 100) {
+			let enemy = new Enemy()
+			enemy.init(5)
+			this.enemys.push(enemy)
+		}
+		this.enemys.forEach((enemy) => {
+			enemy.update()
+			if (enemy.isPlaying) {
+				enemy.aniRender(ctx)
+			}
+		})
+	}
+
+	playerUpdate() {
+		if (frame % 20 == 0) {
+			this.player.shoot()
+		}
+	}
+	generateEnemy() {
+
+	}
+
+	loop() {
+		frame++
+
+		this.aniId = window.requestAnimationFrame(
+			this.bindLoop,
+			canvas
+		)
+	}
+}
